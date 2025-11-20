0018198587257d0c75f1bea78842f00e0950fbf6
Zainab Benelhedi <zainabbenelhedi5@gmail.com>
2025-11-20 21:44:12 +0100

    Starting fresh project
    We have written some test program and i deleted it to start the work

--- Application2.cpp
+++ Application2.cpp
@@ -1,35 +1,9 @@
-// Application2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 using namespace std;
 int pos(string ch,  int c);
 int main()
 {
-    int m[5][6];
-    int t[30];
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; i < 6; j++) {
-            cout << "enter m[" << i << "][" << j << "] :";
-            cin >> m[i][j];
-        } 
-    }
-    int l = 0;
-    string ch = "";
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; i < 6; j++) {
-            if (pos(ch,  m[i][j]) == 0) {
-                t[l] = m[i][j];
-                ch=ch+stoi (m[i][j])+",";
-            }
-        }
-    }
-    return 0;
-}
-int rayen () {
-    cout << "i am cool";
+    cout << "Hello World!";
 }
 
-
-//this is a test
