9350decd7aadf2a28fbc11f60de91c924a20655f
Zainab Benelhedi <zainabbenelhedi5@gmail.com>
2025-11-23 10:59:53 +0100

    Add function to display earliest recruited employee
    Added a function to display the earliest recruited employee.

--- employeeMeth.h
+++ employeeMeth.h
@@ -5,9 +5,11 @@
 void printEmployee(const Employee& e);
 void addEmployee(Employee employees[], int& count, int MAX);
 bool deleteEmployee(Employee employees[], int& count, int id);
+void displayEarliestRecruitedEmployee(Employee employees[], int count);
 
 bool isIdUnique(Employee employees[], int count, int id);
 bool isValidName(const string& name);
 bool isValidHireDate(const string& hiredate);
 int findEmployeeIndex(Employee employees[], int count, int id);
 #endif 
+
