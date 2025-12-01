df912c8ccb24f9163db34775f3ae6e2f4cfe7d29
Zainab Benelhedi <zainabbenelhedi5@gmail.com>
2025-12-01 11:15:47 +0100

    Enhance employee methods with new functionalities
    Added functions to modify and display employees by last name and branch.

--- employeeMeth.h
+++ employeeMeth.h
@@ -1,15 +1,20 @@
 #ifndef EMPLOYEEMETH_H
 #define EMPLOYEEMETH_H
-
 #include "employee.h"
+
 void printEmployee(const Employee& e);
 void addEmployee(Employee employees[], int& count, int MAX);
 bool deleteEmployee(Employee employees[], int& count, int id);
+bool modifyEmployee(Employee employees[], int count, int id);
 void displayEarliestRecruitedEmployee(Employee employees[], int count);
+void DisplayemployeesSortedByLastName(Employee employees[], int count);
+void DisplayEmployeesByBranch(Employee employees[], int count);
 
 bool isIdUnique(Employee employees[], int count, int id);
 bool isValidName(const string& name);
 bool isValidHireDate(const string& hiredate);
 int findEmployeeIndex(Employee employees[], int count, int id);
+
 #endif 
+
 
